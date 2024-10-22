4159c589c9dacf7b664cda170cfb2250b73c2bdc
GustavJaner <gustav.janer@gmail.com>
2024-10-22 16:55:14 +0200

    Init add go metric producer and docker otelc

---
+++ telemetry-producers/otel-go-sdk/test-dice/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/metric/metricdata"
+	"go.opentelemetry.io/otel/sdk/resource"
+)
+
+func main() {
+	// 1. Create resource
+	res, err := newResource()
+	if err != nil {
+		panic(err)
+	}
+
+	// 2. Create a meter provider.
+	grpc := true // False will use the stdout logger as exporter. True will use the otlp gRPC exporter.
+	meterProvider, err := newMeterProvider(res, grpc)
+	if err != nil {
+		panic(err)
+	}
+
+	// 3. Handle shutdown properly so nothing leaks.
+	defer func() {
+		if err := meterProvider.Shutdown(context.Background()); err != nil {
+			log.Println(err)
+		}
+		log.Println("Shutting down meter provider")
+	}()
+
+	// 3. Register as global meter provider so that it can be used via otel.Meter
+	// and accessed using otel.GetMeterProvider.
+	otel.SetMeterProvider(meterProvider)
+
+	http.HandleFunc("/rolldice", rolldice)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func newResource() (*resource.Resource, error) {
+	return resource.New(context.Background(),
+		resource.WithAttributes(
+			attribute.String("service.name", "service-foo"),
+		),
+	)
+}
+
+func newMeterProvider(res *resource.Resource, grpc bool) (*metric.MeterProvider, error) {
+	ctx := context.Background() // Create a context for the gRPC exporter client
+	var metricExporter metric.Exporter
+	var err error
+
+	if grpc == true {
+		log.Println("Using gRPC exporter")
+		metricExporter, err = otlpmetricgrpc.New(
+			ctx,
+			otlpmetricgrpc.WithEndpoint("localhost:4317"),
+			otlpmetricgrpc.WithInsecure(),
+			otlpmetricgrpc.WithTemporalitySelector(func(kind metric.InstrumentKind) metricdata.Temporality { return metricdata.DeltaTemporality }),
+		)
+	} else {
+		log.Println("Using stdout exporter")
+		metricExporter, err = stdoutmetric.New( // stdout for debug purposes. Otherwise we use otlpmetricgrpc to export metrics to the OTEL Collector
+			stdoutmetric.WithPrettyPrint(),
+			stdoutmetric.WithTemporalitySelector(func(kind metric.InstrumentKind) metricdata.Temporality { return metricdata.DeltaTemporality }),
+		)
+
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	meterProvider := metric.NewMeterProvider(
+		metric.WithResource(res),
+		metric.WithReader(metric.NewPeriodicReader(metricExporter, metric.WithInterval(5*time.Second))), // PeriodicReader is a Reader that continuously collects and exports metric data at a set interval.
+	)
+	return meterProvider, nil
+}
---
+++ telemetry-producers/otel-go-sdk/test-dice/rolldice.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
+)
+
+const name = "dice-meter"
+
+var (
+	// 4. Create a Meter from the Meter Provider
+	meter = otel.Meter(name)
+	// 5. Create a counter from the Meter
+	rollCount metric.Int64Counter
+	rollSum   metric.Int64Counter
+)
+
+func init() {
+	var err error
+	rollCount, err = meter.Int64Counter("dice_rolls_total",
+		metric.WithDescription("Total number of dice rolls"),
+		metric.WithUnit("{roll}"))
+	if err != nil {
+		panic(err)
+	}
+
+	rollSum, err = meter.Int64Counter("dice_rolls_value_total",
+		metric.WithDescription("Total sum of dice roll values"),
+		metric.WithUnit("{roll}"))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func rolldice(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	roll := 1 + rand.Intn(6)
+
+	log.Print("Rolled: ", roll)
+
+	rollCount.Add(ctx, 1, metric.WithAttributes(attribute.String("endpoint", "rolldice")))
+	rollSum.Add(ctx, int64(roll), metric.WithAttributes(attribute.String("endpoint", "rolldice")))
+	rollSum.Add(ctx, 1, metric.WithAttributes(attribute.String("endpoint", "rolldice"), attribute.String("test", "foo")))
+
+	resp := strconv.Itoa(roll) + "\n"
+	if _, err := io.WriteString(w, resp); err != nil {
+		log.Printf("Write failed: %v\n", err)
+	}
+}
