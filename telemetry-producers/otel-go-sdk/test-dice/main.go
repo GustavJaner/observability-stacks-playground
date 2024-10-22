4a6dc37b7e4525f3c16b574a924db6b569f1a573
GustavJaner <gustav.janer@gmail.com>
2024-10-22 23:18:34 +0200

    Add grafana dashboard

--- telemetry-producers/otel-go-sdk/test-dice/main.go
+++ telemetry-producers/otel-go-sdk/test-dice/main.go
@@ -50,9 +50,16 @@ func main() {
 }
 
 func newResource() (*resource.Resource, error) {
-	return resource.New(context.Background(),
+	return resource.New(
+		context.Background(),
+		resource.WithFromEnv(),      // Discover and provide attributes from OTEL_RESOURCE_ATTRIBUTES and OTEL_SERVICE_NAME environment variables.
+		resource.WithTelemetrySDK(), // Discover and provide information about the OpenTelemetry SDK used.
+		resource.WithContainer(),    // Discover and provide container information.
+		// resource.WithProcess(),      // Discover and provide process information.
+		// resource.WithOS(),           // Discover and provide OS information.
+		// resource.WithHost(),         // Discover and provide host information.
 		resource.WithAttributes(
-			attribute.String("service.name", "service-foo"),
+			attribute.Key("service.name").String("dice-service"), // Add custom resource attributes.
 		),
 	)
 }
